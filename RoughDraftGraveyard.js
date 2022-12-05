995d8ca8b3ba71a7deee6b956135b39d343a0c62
Bailey <baileyrosefern@gmail.com>
2022-12-05 15:20:15 -0700

    First commit-working on some css and making a dice roller

---
+++ RoughDraftGraveyard.js
@@ -0,0 +1,4 @@
+//  <img src = "../images/diceroller/dice3.png" id = "die 3">
+{/* <img src = "../images/diceroller/dice4.png" id = "die-4">
+<img src = "../images/diceroller/dice5.png" id = "die-5">
+<img src = "../images/diceroller/dice6.png" id = "die-6"> */}
---
+++ pages/actions/diceroller.js
@@ -0,0 +1,9 @@
+// function rollDice() {
+//     const img = document.getElementById("img");
+//     let randomNumber = Math.floor(Math.random() * 6) +1;
+//     console.log(randomNumber);
+//     img.setAttribute("src", `${randomNumber}.png`);
+// }
+
+let images = ["images/diceroller/dice1.png", ""]
+
