e5a675c967d25c0bac04dbf4c5a80a23a244d45d
Bailey <baileyrosefern@gmail.com>
2022-12-05 15:43:01 -0700

    Got dice roller to work, need to cycle through images of dice next

--- pages/actions/diceroller.js
+++ pages/actions/diceroller.js
@@ -5,5 +5,24 @@
 //     img.setAttribute("src", `${randomNumber}.png`);
 // }
 
-let images = ["images/diceroller/dice1.png", ""]
+let images = ["images/diceroller/dice1.png", "images/diceroller/dice2.png",
+"images/diceroller/dice3.png", "images/diceroller/dice4.png", "images/diceroller/dice5.png",
+"images/diceroller/dice6.png"];
+let dice = document.querySelectorAll("img");
+
+function roll(){
+    dice.forEach(function(die) {
+        die.classList.add("shake");
+    });
+    setTimeout(function() {
+        dice.forEach(function(die) {
+            die.classList.remove("shake");
+        });
+        let dieOneValue = Math.floor(Math.random() * 6);
+        let dieTwoValue = Math.floor(Math.random() * 6); 
+        console.log(dieOneValue, dieTwoValue)
+    },
+    1000
+    );
+}
 
